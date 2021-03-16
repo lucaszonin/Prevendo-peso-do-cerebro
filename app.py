d0c14f33ec9d8e1dc6e84973c8e2997d82cf9162
Lucas Zonin Soares <47160733+lucaszonin@users.noreply.github.com>
2021-03-15 23:54:03 -0300

    Update app.py
    alterei alguns parâmetros

--- app.py
+++ app.py
@@ -1,47 +1,50 @@
 import pandas as pd
 
-class LinearRegression:
+class linear_regression:
+    
     '''
       Prevendo o peso do cérebro considerando o tamanho da cabeça de seres humanos utilizando Regressão Linear Simples.
     '''
-    dados = pd.read_csv('pesos2.csv')
-    
-    #Separando os dados
-    X = dados['Head Size'].values
-    Y = dados['Brain Weight'].values
 
-    numerador = 0
-    denominador = 0
-
-    def __init__(self, X = X, Y = Y):
+    #Carregar os dados
+    dados = pd.read_csv('dados/pesos2.csv')
     
-        self.X = X
-        self.Y = Y
-        self.n = len(X)
+    #Preparando as informações iniciais
+    def __init__(self):
+    
+        self.numerador = 0
+        self.denominador = 0
+        self.X = self.dados['Head Size'].values
+        self.Y = self.dados['Brain Weight'].values
+        self.n = len(self.X)
         self.X_mean = sum(self.X) / len(self.X)
         self.Y_mean = sum(self.Y) / len(self.Y)
         self.head_size = input('Digite o tamanho em cm³ da cabeça: ')
-    
+        self.calculos_ab()
+
+    #Fórmula regressão linear simples para calcular a e b 
+    def calculos_ab(self):
+
         for i in range(self.n):
 
-            self.numerador += (X[i] - self.X_mean) * (Y[i] - self.Y_mean)
-            self.denominador += (X[i] - self.X_mean) ** 2
+            self.numerador += (self.X[i] - self.X_mean) * (self.Y[i] - self.Y_mean)
+            self.denominador += (self.X[i] - self.X_mean) ** 2
                 
-        self.b = self.numerador / self.denominador
-        self.a = self.Y_mean - (self.b * self.X_mean)
+        b = self.numerador / self.denominador
+        a = self.Y_mean - (b * self.X_mean)
 
-        return self.prever_peso(self.a, self.b, self.head_size)
+        self.prever_peso(a, b)
 
 
-    def prever_peso(self, a, b, head_size):
+    def prever_peso(self, a, b):
 
         # y = a + bx
     
-        y = a + (b * int(head_size))
+        y = a + (b * int(self.head_size))
  
         return print("O peso do cérebro em gramas é: {}".format(y))
 
 
 if __name__ == "__main__":
     
-    model = LinearRegression()
+    modelo  = linear_regression()
